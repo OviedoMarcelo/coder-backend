758b754d34117a2891762bfccac9a121551fa81a
Marcelo Oviedo <oviedommarcelo@gmail.com>
2023-03-25 19:34:25 -0300

    Fix errors and testing

--- productManager.js
+++ productManager.js
@@ -22,10 +22,22 @@ class ProductManager {
 
         };
         //Set product.id
-        this.products.length === 0 ? product.id = 1 : product.id = this.products[this.product.length - 1].id + 1;
+        this.products.length === 0 ? product.id = 1 : product.id = this.products[this.products.length - 1].id + 1;
+
+        if (
+            !title ||
+            !description ||
+            !price ||
+            !thumbail ||
+            !code ||
+            !stock            
+            ){
+                console.log('No se pudo agregar el producto. Todos los campos son obligatorios, verificar los mismos.')
+                return;
+            }
 
         //Existing product code validator
-        const productCodeIndex = this.products.findIndex(product => product.code === this.code);
+        const productCodeIndex = this.products.findIndex((product) => product.code === code);
         if (productCodeIndex === -1) {
             this.products.push(product)
             console.log('Producto agregado correctamente :)')
@@ -50,3 +62,37 @@ class ProductManager {
 
 }
 
+////Test space
+
+const product = new ProductManager();
+
+console.log('Todos los productos iniciales')
+console.log(product.getProducts());
+console.log('---------------------------')
+
+console.log('Agrego el primer producto')
+product.addProduct("Tomate rojo", "El mejor tomate", 200, "Sin imagen", "001", 25)
+console.log(product.getProducts());
+console.log('---------------------------')
+
+console.log('Agrego el segundo producto')
+product.addProduct("Lechuga", "La más rica y fresca", 400, "Sin imagen", "002", 10);
+console.log(product.getProducts());
+console.log('---------------------------')
+
+console.log('Agrego un producto con campos incompletos')
+product.addProduct("Lechuga", 400, "Sin imagen", "002",);
+console.log(product.getProducts());
+console.log('---------------------------')
+
+console.log('Agrego un producto con el code repetido')
+product.addProduct("Tomete amarillo", "Otro tomate", 250, "Sin imagen", "001", 8);
+console.log(product.getProducts());
+console.log('---------------------------')
+
+console.log('Muestro el producto con ID 4 que no existente')
+console.log(product.getProductByID(4));
+console.log('---------------------------')
+
+console.log('Muestro el producto con ID 1 existente')
+console.log(product.getProductByID(1));
