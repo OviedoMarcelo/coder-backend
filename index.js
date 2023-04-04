c2781a3fc72cdc4707975a8eea9c7fa3c59876d2
Marcelo Oviedo <oviedommarcelo@gmail.com>
2023-04-04 18:54:26 -0300

    Add test case and fix some error msg

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const product = new ProductManager(path);
 (async () => {
 
     try {
+        //Product list empty
         const productlist = await product.getProducts();
         console.log('Todos los productos iniciales');
         console.log(productlist);
@@ -20,7 +21,8 @@ const product = new ProductManager(path);
             code: "001",
             stock: 25,
         };
-        
+
+        //add new product
         console.log('Agrego el primer producto');
         await product.addProduct(newProduct);
         const newProductlist = await product.getProducts();
@@ -35,21 +37,40 @@ const product = new ProductManager(path);
             stock: 10,
         };
 
+        //add a second one product
         await product.addProduct(newProduct2);
         console.log('---------------------------')
         console.log('Inserto nuevo producto')
         const newProductlist2 = await product.getProducts();
         console.log(newProductlist2);
 
+        //Update a product
+
         const productToUpdate = {
             title: "Lechuga morada",
-            description:"Morada y sabrosa",
+            description: "Morada y sabrosa",
             price: 500,
         }
 
-        await product.updateProduct(2,productToUpdate);
+        await product.updateProduct(2, productToUpdate);
         const newProductlist3 = await product.getProducts();
         console.log(newProductlist3);
+
+        //delete a product
+        await product.deleteProduct(2)
+        const newProductlist4 = await product.getProducts();
+        console.log(newProductlist4);
+
+
+        //show a product id 1 and id 2 recently deleted 
+        const newProductlist5 = await product.getProductByID(1);
+        console.log('---------------------------');
+        console.log('Product con ID 1');
+        newProductlist5 ? console.log(newProductlist5) : console.log('No se encontró el producto con el ID solicitado');
+        const newProductlist6 = await product.getProductByID(2);
+        console.log('---------------------------');
+        console.log('Product con ID 2');
+        newProductlist6 ? console.log(newProductlist6) : console.log('No se encontró el producto con el ID solicitado');
 
 
     } catch (error) {
